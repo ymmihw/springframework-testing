9905ac3e6cd84693b0b03389b862f1598344a972
10004160 <10004160@win10-tjgry08.ctfo.local>
2021-04-23 16:29:53 +0800

    @EnableJpaRepositories

--- dynamicproperties/src/main/java/com/ymmihw/springframework/DynamicPropertiesApplication.java
+++ dynamicproperties/src/main/java/com/ymmihw/springframework/DynamicPropertiesApplication.java
@@ -2,11 +2,13 @@ package com.ymmihw.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class DynamicPropertiesApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DynamicPropertiesApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DynamicPropertiesApplication.class, args);
+  }
 }
