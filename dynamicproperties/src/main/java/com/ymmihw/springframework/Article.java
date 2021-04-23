dd82baa0979ce27e50f95a1b9245f1ce6b95f2a2
10004160 <10004160@win10-tjgry08.ctfo.local>
2021-04-23 16:37:19 +0800

    show sql

--- dynamicproperties/src/main/java/com/ymmihw/springframework/Article.java
+++ dynamicproperties/src/main/java/com/ymmihw/springframework/Article.java
@@ -1,45 +1,23 @@
 package com.ymmihw.springframework;
 
+import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import static javax.persistence.GenerationType.IDENTITY;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "articles")
+@Getter
+@Setter
 public class Article {
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	private Long id;
+  private String title;
 
-	private String title;
-
-	private String content;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
+  private String content;
 }
